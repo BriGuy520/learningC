558444ffe4acc3e7a60313a9dac955183a62a7ab
Brian Falasz <bfalasz@yahoo.com>
2021-03-10 23:47:12 -0600

    tri diagonal matrix problem solved

--- Data Structures/Matrices/triDiagonal.c
+++ Data Structures/Matrices/triDiagonal.c
@@ -11,11 +11,11 @@ struct Array {
 void Set(struct Array *arr, int i, int j, int x){
   
   if(i == j){
-    arr->A[arr->size - 1 + i] = x;
+    arr->A[arr->size - 1 + i - 1] = x;
   } else if(i - j == 1){
-    arr->A[2 * arr->size - 1 + i] = x;
-  } else if(i - j == -1){
-    arr->A[i - 1] = x;
+    arr->A[j - 1] = x;
+  } else if (i - j == -1){
+    arr->A[(2 * arr->size) - 1 + i - 1] = x;
   }
 }
 
@@ -26,11 +26,13 @@ void Display(struct Array arr){
   for(i = 1; i <= arr.size; i++){
     for(j = 1; j <= arr.size;j++){
       if(i == j){
-        printf("%d ", arr.A[arr.size - 1 + i]);
-      } else if(i - j == 1) {
-        printf("%d ", arr.A[2 * arr.size - 1 + i]);
-      } else if (i - j == -1){
-        printf("%d", arr.A[i - 1]);
+        printf("%d ", arr.A[arr.size - 1 + i - 1]);
+      } else if(i - j == 1){
+        printf("%d ", arr.A[j - 1]);
+      } else if(i - j == -1){
+        printf("%d ", arr.A[(2 * arr.size) - 1 + i - 1]);
+      } else {
+        printf("0 ");
       }
     }
     printf("\n");
@@ -45,17 +47,16 @@ int main(){
 
   printf("Enter Dimension: ");
   scanf("%d", &arr.size);
-  arr.A = (int *) malloc(3 * arr.size - 2 * sizeof(int));
+  arr.A = (int *) malloc((3 * arr.size - 2) * sizeof(int));
 
   printf("Enter Integers in %d x %d TriDiagonal Matrix:\n", arr.size, arr.size);
-  for(i = 1; i  <= arr.size;i++){
+  for(i = 1; i <= arr.size;i++){
     for(j = 1; j <= arr.size;j++){
       scanf("%d", &x);
       Set(&arr, i, j, x);
-      printf("i: %d, j: %d \n", i, j);
     }
   }
 
-  printf("\n\nthis is called");
+  printf("\n\n");
   Display(arr);
 }
