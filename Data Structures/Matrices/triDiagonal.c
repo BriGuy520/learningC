f60e1885d261d799da46111e55d7f9ab6f27a613
Brian Falasz <bfalasz@yahoo.com>
2021-03-10 22:03:05 -0600

    Making progress in triDiagonal Matrix problem

--- Data Structures/Matrices/triDiagonal.c
+++ Data Structures/Matrices/triDiagonal.c
@@ -11,10 +11,12 @@ struct Array {
 void Set(struct Array *arr, int i, int j, int x){
   
   if(i == j){
-    arr->A[i + j + 1] = x;
+    arr->A[arr->size - 1 + i] = x;
   } else if(i - j == 1){
-    arr->A[i + j - arr->size - 1] = x;
-  } 
+    arr->A[2 * arr->size - 1 + i] = x;
+  } else if(i - j == -1){
+    arr->A[i - 1] = x;
+  }
 }
 
 void Display(struct Array arr){
@@ -24,11 +26,11 @@ void Display(struct Array arr){
   for(i = 1; i <= arr.size; i++){
     for(j = 1; j <= arr.size;j++){
       if(i == j){
-        printf("%d ", arr.A[i + j + 1]);
+        printf("%d ", arr.A[arr.size - 1 + i]);
       } else if(i - j == 1) {
-        printf("%d ", arr.A[i + j - 1]);
-      } else{
-        printf("0 ");
+        printf("%d ", arr.A[2 * arr.size - 1 + i]);
+      } else if (i - j == -1){
+        printf("%d", arr.A[i - 1]);
       }
     }
     printf("\n");
@@ -43,16 +45,17 @@ int main(){
 
   printf("Enter Dimension: ");
   scanf("%d", &arr.size);
-  arr.A = (int *) malloc(arr.size * (arr.size - 1) / 2 * sizeof(int));
+  arr.A = (int *) malloc(3 * arr.size - 2 * sizeof(int));
 
   printf("Enter Integers in %d x %d TriDiagonal Matrix:\n", arr.size, arr.size);
-  for(i = 1; i <=arr.size;i++){
-    for(j = 1; j <=arr.size;j++){
+  for(i = 1; i  <= arr.size;i++){
+    for(j = 1; j <= arr.size;j++){
       scanf("%d", &x);
       Set(&arr, i, j, x);
+      printf("i: %d, j: %d \n", i, j);
     }
   }
 
-  printf("\n\n");
+  printf("\n\nthis is called");
   Display(arr);
 }
